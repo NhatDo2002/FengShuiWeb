e1d40bd3f47998436b7f77cf44ae0d19e530741f
Đỗ Minh Nhật <87433277+NhatDo2002@users.noreply.github.com>
2025-02-06 17:04:13 +0700

    úp đết

---
+++ Assets/JS/home.js
@@ -0,0 +1,17 @@
+﻿var globalImgTop;
+
+$(document).ready(function () {
+    var $image = $("#side-image");
+    globalImgTop = $image.offset().top;
+    $(window).on("scroll", function () {
+        var $image = $("#side-image");
+        var scrollTop = $(window).scrollTop(); // Vị trí cuộn của cửa sổ
+        console.log(scrollTop, globalImgTop)
+        if (scrollTop >= globalImgTop) {
+            $image.addClass("make-fixed");
+        } else {
+            $image.removeClass("make-fixed");
+        }
+    });
+});
+
